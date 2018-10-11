7f462e76ecbdd5119ca01cc2b984e10d08e12489
Takumi Watanabe <takumiwatanabe@TakuminoMacBook-puro.local>
2018-10-12 01:40:50 +0900

    add some objects

---
+++ Roll-a-Ball/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    // ターゲットへの参照
+    public Transform target;
+
+    // MainCameraとPlayerとの相対距離
+    private Vector3 offset;
+
+	void Start()
+	{
+        // 自分自身(MainCamera)とtargetとの相対距離を算出
+        offset = GetComponent<Transform>().position - target.position;
+	}
+
+	void Update () {
+        // 自分の座標にtargetの座標を代入する
+        GetComponent<Transform>().position = target.position + offset;
+	}
+}
---
+++ Roll-a-Ball/Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+	
+	// トリガーとの接触時に呼ばれるコールバック
+	void OnTriggerEnter (Collider hit) {
+		
+        // 接触対象がPlayerタグかどうか
+        if (hit.CompareTag ("Player")){
+            // このコンポーネント(Item)を持つGameObjectを破棄する
+            Destroy(gameObject);
+        }
+	}
+}
