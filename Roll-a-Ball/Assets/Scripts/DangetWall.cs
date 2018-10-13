373c57a58e0ee0e6def74a6191b413efaf03add6
Takumi Watanabe <takumiwatanabe@TakuminoMacBook-puro.local>
2018-10-13 21:31:21 +0900

    complete

---
+++ Roll-a-Ball/Assets/Scripts/DangetWall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DangetWall : MonoBehaviour {
+
+	//public GameObject loserLabelObject;
+
+	// オブジェクトと接触した時に呼ばれるコールバック
+	void OnCollisionEnter (Collision hit) {
+
+		// 接触したオブジェクトのタグが"Player"の場合
+		if (hit.gameObject.CompareTag ("Player")){
+
+　　　　//loserLabelObject.SetActive(true);
+
+			// 現在のシーン番号を取得
+			int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+			// 現在のシーンを再読み込みする
+			SceneManager.LoadScene(sceneIndex);
+		}
+	}
+}
---
+++ Roll-a-Ball/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public UnityEngine.UI.Text scoreLabel;
+    public GameObject winnerLabelObject;
+    //public GameObject loserLabelObject;
+
+	public void Update () {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+        scoreLabel.text = count.ToString();
+
+        if (count == 0){
+            // オブジェクト(winnerLabelObject)をアクティブにする
+            winnerLabelObject.SetActive(true);
+        }
+	}
+}
