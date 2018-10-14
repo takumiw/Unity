04a7fd73c42455a08b66d655103f6f7c3f55653c
Takumi Watanabe <takumiwatanabe@TakuminoMacBook-puro.local>
2018-10-14 19:53:04 +0900

    add and complete Breakout game

---
+++ Breakout/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour {
+
+	public int boxNum;
+	public float currentTime;
+
+	// Use this for initialization
+	void Start () {
+		currentTime = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		currentTime += Time.deltaTime;
+		if (boxNum <= 0){
+			GameOver(currentTime.ToString("F0") + "秒でクリア！");
+		}
+	}
+
+	public void GameOver(string resultMessage){
+		DataSender.resultMessage = resultMessage;
+		SceneManager.LoadScene("Result");
+	}
+}
